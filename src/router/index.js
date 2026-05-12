b34212b159f4d46722cbb9b673c7408d76971762
keson <keson>
2026-05-12 13:33:03 +0900

    화면 이동시 상단 이동

--- src/router/index.js
+++ src/router/index.js
@@ -17,7 +17,14 @@ const routes = [
 
 const router = createRouter({
   history: createWebHistory(),
-  routes
+  routes,
+  scrollBehavior(to, from, savedPosition){
+    if (savedPosition) {
+      return savedPosition
+    } else {
+      return { top: 0 }
+    }
+  }
 })
 
 // 👮‍♂️ 라우터 보초병 (Navigation Guard)
